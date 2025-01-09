30d38484c065802065b2319c55d3c2384871e3f7
jhonatapers <jhonatapers@gmail.com>
2025-01-09 10:37:29 -0300

    Add infrastructure module with Spring Boot setup and initial configuration

---
+++ infrastructure/src/main/java/com/callv2/drive/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,10 @@
+package com.callv2.drive.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.callv2.drive")
+public class WebServerConfig {
+
+}
