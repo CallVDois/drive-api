3367f222f82b21d20a5b7feb63d80631ca5c9ecf
jhonatapers <jhonatapers@gmail.com>
2025-01-13 12:07:08 -0300

    Implement validation for Content fields: add checks for non-null and non-empty location and type

--- domain/src/main/java/com/callv2/drive/domain/file/Content.java
+++ domain/src/main/java/com/callv2/drive/domain/file/Content.java
@@ -2,6 +2,7 @@ package com.callv2.drive.domain.file;
 
 import com.callv2.drive.domain.ValueObject;
 import com.callv2.drive.domain.validation.ValidationHandler;
+import com.callv2.drive.domain.validation.Error;
 
 public record Content(String location, String type, long size) implements ValueObject {
 
@@ -10,8 +11,32 @@ public record Content(String location, String type, long size) implements ValueO
     }
 
     @Override
-    public void validate(ValidationHandler aHandler) {
-        // TODO implements validation
+    public void validate(final ValidationHandler aHandler) {
+        validateLocation(aHandler);
+        validateType(aHandler);
     }
 
+    private void validateLocation(final ValidationHandler aHandler) {
+        if (location == null) {
+            aHandler.append(new Error("'location' cannot be null."));
+            return;
+        }
+
+        if (location.trim().isEmpty()) {
+            aHandler.append(new Error("'location' cannot be empty."));
+            return;
+        }
+    }
+
+    private void validateType(final ValidationHandler aHandler) {
+        if (type == null) {
+            aHandler.append(new Error("'type' cannot be null."));
+            return;
+        }
+
+        if (type.trim().isEmpty()) {
+            aHandler.append(new Error("'type' cannot be empty."));
+            return;
+        }
+    }
 }
