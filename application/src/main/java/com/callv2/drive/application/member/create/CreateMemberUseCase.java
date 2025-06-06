c92564937b50db0fc813b03b39eadc197b48e960
jhonatapers <jhonatapers@gmail.com>
2025-06-06 13:12:29 -0300

    feat: implement CreateMemberInput and DefaultCreateMemberUseCase for member creation logic
    Co-authored-by: Artur S <Artituis@users.noreply.github.com>

---
+++ application/src/main/java/com/callv2/drive/application/member/create/CreateMemberUseCase.java
@@ -0,0 +1,7 @@
+package com.callv2.drive.application.member.create;
+
+import com.callv2.drive.application.UnitUseCase;
+
+public abstract class CreateMemberUseCase extends UnitUseCase<CreateMemberInput> {
+
+}
