c6eb38417577f32bc14662386e4ea8f4ad4a7276
jhonatapers <jhonatapers@gmail.com>
2025-06-10 10:10:01 -0300

    style: adjust indentation in DeleteFileInput for consistency

--- application/src/main/java/com/callv2/drive/application/file/delete/DeleteFileInput.java
+++ application/src/main/java/com/callv2/drive/application/file/delete/DeleteFileInput.java
@@ -4,7 +4,8 @@ import java.util.UUID;
 
 public record DeleteFileInput(String deleterId, UUID fileId) {
 
-        public static DeleteFileInput of(final String deleterId, final UUID fileId) {
-            return new DeleteFileInput(deleterId, fileId);
-        }
+    public static DeleteFileInput of(final String deleterId, final UUID fileId) {
+        return new DeleteFileInput(deleterId, fileId);
+    }
+
 }
