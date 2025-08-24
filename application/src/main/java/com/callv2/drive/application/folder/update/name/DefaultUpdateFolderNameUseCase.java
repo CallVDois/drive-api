eeb9b0d0f1c95e9d35ef035b88f61d042aabebdf
jhonatapers <jhonatapers@gmail.com>
2025-08-23 23:08:06 -0300

    refactor: streamline folder name update logic and improve validation checks

--- application/src/main/java/com/callv2/drive/application/folder/update/name/DefaultUpdateFolderNameUseCase.java
+++ application/src/main/java/com/callv2/drive/application/folder/update/name/DefaultUpdateFolderNameUseCase.java
@@ -1,5 +1,7 @@
 package com.callv2.drive.application.folder.update.name;
 
+import java.util.Set;
+
 import com.callv2.drive.domain.exception.NotFoundException;
 import com.callv2.drive.domain.exception.ValidationException;
 import com.callv2.drive.domain.folder.Folder;
@@ -24,25 +26,19 @@ public class DefaultUpdateFolderNameUseCase extends UpdateFolderNameUseCase {
                 .findById(FolderID.of(input.folderId()))
                 .orElseThrow(() -> NotFoundException.with(Folder.class, input.folderId().toString()));
 
-        final Folder parentFolder = this.folderGateway
-                .findById(folder.getParentFolder())
-                .orElseThrow(
-                        () -> NotFoundException.with(Folder.class, folder.getParentFolder().getValue().toString()));
-
         final FolderName folderName = FolderName.of(input.name());
-        validateFolderName(parentFolder, folderName);
-
-        this.folderGateway.update(folder.changeName(folderName));
-    }
-
-    private void validateFolderName(final Folder parentFolder, final FolderName name) {
 
         final Notification notification = Notification.create();
 
-        if (parentFolder.getSubFolders().stream().anyMatch(subFolder -> subFolder.name().equals(name)))
+        final Set<Folder> subFolders = folderGateway.findByParentFolderId(folder.getParentFolder());
+
+        if (subFolders.stream().anyMatch(subFolder -> subFolder.getName().equals(folderName)))
             notification.append(ValidationError.with("Folder with the same name already exists"));
 
         if (notification.hasError())
             throw ValidationException.with("Could not update folder name", notification);
+
+        this.folderGateway.update(folder.changeName(folderName));
     }
+
 }
