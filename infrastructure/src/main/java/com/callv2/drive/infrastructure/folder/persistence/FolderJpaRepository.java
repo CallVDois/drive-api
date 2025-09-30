908099109d8604dc4bce81ded49c46015499a433
jhonatapers <jhonatapers@gmail.com>
2025-09-30 13:23:00 -0300

    refactor: remove unused imports and commented-out code in FolderJpaRepository

--- infrastructure/src/main/java/com/callv2/drive/infrastructure/folder/persistence/FolderJpaRepository.java
+++ infrastructure/src/main/java/com/callv2/drive/infrastructure/folder/persistence/FolderJpaRepository.java
@@ -4,13 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.lang.NonNull;
-import org.springframework.lang.Nullable;
 
 public interface FolderJpaRepository extends
         JpaRepository<FolderJpaEntity, UUID>,
@@ -19,8 +14,5 @@ public interface FolderJpaRepository extends
     Optional<FolderJpaEntity> findByRootFolderTrueAndOwnerId(UUID ownerId);
 
     List<FolderJpaEntity> findAllByParentFolderId(UUID parentFolderId);
-
-    // @NonNull
-    // Page<FolderJpaEntity> findAll(@Nullable Specification<FolderJpaEntity> whereClause, @NonNull Pageable page);
 
 }
