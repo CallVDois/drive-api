6bffad5cf5293bc7be1a2cfb256ff81bfd807ba0
jhonatapers <jhonatapers@gmail.com>
2025-10-05 13:14:14 -0300

    refactor: Remove unused FileSharingReceivedListInput, FileSharingReceivedListOutput, and ListFileSharingReceivedUseCase classes

--- application/src/main/java/com/callv2/drive/application/sharing/file/retrieve/list/received/ListFileSharingReceivedUseCase.java
+++
@@ -1,8 +0,0 @@
-package com.callv2.drive.application.sharing.file.retrieve.list.received;
-
-import com.callv2.drive.application.UseCase;
-
-public abstract class ListFileSharingReceivedUseCase
-        extends UseCase<FileSharingReceivedListInput, FileSharingReceivedListOutput> {
-
-}
