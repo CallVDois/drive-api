8cc6fe27d9c8eb4c09ca2071f83e85e520418898
jhonatapers <jhonatapers@gmail.com>
2025-10-10 17:30:03 -0300

    refactor: simplify member existence validation in folder provisioning methods

--- domain/src/main/java/com/callv2/drive/domain/folder/service/FolderProvisioningService.java
+++ domain/src/main/java/com/callv2/drive/domain/folder/service/FolderProvisioningService.java
@@ -32,13 +32,27 @@ public class FolderProvisioningService {
 
     public Folder provisionRootFolder(final MemberID owner) {
 
-        if (Boolean.FALSE.equals(memberGateway.existsById(owner)))
-            throw NotFoundException.with(Member.class, owner.getValue().toString());
+        validateMemberExists(owner);
 
         return folderGateway
                 .findMemberRootFolder(owner)
                 .orElseGet(() -> createFolderAcl(Folder.createRoot(owner)));
 
+    }
+
+    public Folder provisionInboxFolder(final MemberID owner) {
+
+        validateMemberExists(owner);
+
+        return folderGateway
+                .findDefaultMemberSharedInbox(owner)
+                .orElseGet(() -> createFolderAcl(Folder.createInbox(owner, this.provisionRootFolder(owner).getId())));
+
+    }
+
+    private void validateMemberExists(final MemberID memberId) {
+        if (Boolean.FALSE.equals(memberGateway.existsById(memberId)))
+            throw NotFoundException.with(Member.class, memberId.getValue().toString());
     }
 
     private Folder createFolderAcl(final Folder folder) {
