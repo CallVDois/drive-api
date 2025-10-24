412c0ad80a47734008e4e5b28fe594449340b66f
jhonatapers <jhonatapers@gmail.com>
2025-10-24 13:21:14 -0300

    test: add a placeholder test to verify test framework setup

--- infrastructure/src/test/java/com/callv2/drive/infrastructure/MainTest.java
+++ infrastructure/src/test/java/com/callv2/drive/infrastructure/MainTest.java
@@ -1,5 +1,13 @@
 package com.callv2.drive.infrastructure;
 
-public class MainTest {
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @Test
+    void dummyTest() {
+        // This is a placeholder test to ensure the test framework is set up correctly.
+        assert true;
+    }
 
 }
